b957cdbbbd455b2f49b808149e898c6aa2935294
dyrstiu <vaatifelix@gmail.com>
2022-02-17 12:51:19 +0300

    This program checks whether the yr is a leap yr or not

--- Leaptester.py
+++ Leaptester.py
@@ -1,3 +1,14 @@
 # Author : @Dyrstiu
 #Description : A program to check whether a yr is a leap yr or not.
 
+year =int(input("Enter year to check: "))
+def leap (year):
+    if (year % 4) == 0 :
+        print("This is a leap year")
+        exit()
+    elif (year % 400) == 0 :
+        print("This is a leap year")
+    else:
+        print("This is not a leap year")
+    
+leap(year)
